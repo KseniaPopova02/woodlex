12ff8032b157e3c7db85d8c8f9074496ee8bce91
ksenia.popova <ksenia.popova>
2024-07-22 14:38:37 +0300

    add: thermo-ash-filling

--- js/main.js
+++ js/main.js
@@ -126,7 +126,7 @@ const translations = {
     termoyasen: "Thermo Ash",
     ekzotika: "Exotic",
     fasadi: "Facades",
-    pidshivka: "Soffits",
+    pidshivka: "Filling",
     interyeri: "Interiors",
     portfolio: "Portfolio",
     eksklyuzivniVirobi: "Exclusive Products",
@@ -145,6 +145,40 @@ const translations = {
     NashiPartneri: "OUR PARTNERS",
     ZKimPrazuemo: "WHO WE WORK WITH",
     aboutUsTitle: "Our guiding principle is «functionalism and space».",
+    thermoAshFillingMatherialFunction: "THERMO-ASH FILLING",
+    contactUActionText: "PLEASE, CONTACT US FOR ALL DETAILS",
+    thermoAshFillingTextAndImageTitle:
+      "ECO-FRIENDLINESS AND DURABILITY OF THE MATERIAL",
+    thermoAshFillingTextAndImageMainText1:
+      "Thermo-ash filling is a very popular choice among many of our clients. Why is that? Because thermo-ash has many advantages and has proven itself well over many years of use.",
+    thermoAshFillingTextAndImageMainText2:
+      "The main advantages of the material:",
+    thermoAshFillingTextAndImageMainText3:
+      "Thermal insulation: Thermo-ash has high thermal insulation properties, which allows keeping the room warm.",
+    thermoAshFillingTextAndImageMainText4:
+      "Eco-friendliness: Thermo-ash is a natural material that does not contain harmful substances and does not release harmful substances.",
+    thermoAshFillingTextAndImageMainText5:
+      "Durability: Thermo-ash holds up well, has high resistance to atmospheric conditions and mechanical damage.",
+    thermoAshFillingTextAndImageMainText6:
+      "Aesthetics: Thermo-ash filling has a natural wooden look, adding naturalness and comfort to the interior.",
+    thermoAshFillingTextAndImageMainText7:
+      "Sound insulation: Thermo-ash absorbs sounds well, allowing for a comfortable acoustic environment in the room.",
+    thermoAshFillingTextAndImageMainText8:
+      "Ease of installation: Thermo-ash is easy to cut and install, simplifying the installation process.",
+    thermoAshFillingTextAndImageMainText9:
+      "Moisture regulation: Thermo-ash has the property of regulating moisture in the room, creating comfortable conditions for the inhabitants.",
+    thermoAshFillingTextAndImageMainText10:
+      "High antiseptic properties: Thermo-ash has excellent antiseptic properties, allowing to maintain the health of the room.",
+    thermoAshFillingTextAndImageMainText11:
+      "Choosing thermo-ash filling can be an excellent solution for creating a comfortable and aesthetic interior in your room.",
+    thermoAshFillingImageAndTextTitle:
+      "MULTIFUNCTIONALITY AND COMFORTABLE INSTALLATION",
+    thermoAshFillingImageAndTextMainText1:
+      "Thermo-ash filling can be used for finishing various types of rooms, making it a versatile choice for any design projects.",
+    thermoAshFillingImageAndTextMainText2:
+      "Installation can be faster compared to other types of filling, saving time during renovation or construction, as the material is lightweight and has a convenient shape for cutting and installation.",
+    thermoAshFillingImageAndTextMainText3:
+      "Thermo-ash material can be painted or treated to achieve the desired design and color. Also, the use of such filling can improve the energy efficiency of the room by retaining heat and reducing heating costs.",
   },
   UA: {
     headerText: "СТУДІЯ ДИЗАЙНУ ІНТЕР'ЄРУ ТА ЕКСТЕР'ЄРУ",
@@ -176,6 +210,39 @@ const translations = {
     NashiPartneri: "НАШІ ПАРТНЕРИ",
     ZKimPrazuemo: "З КИМ МИ ПРАЦЮЕМО",
     aboutUsTitle: "Наш керівний принцип – «функціоналізм і простір».",
+    thermoAshFillingMatherialFunctionB: "ПІДШИВКА З ТЕРМОЯСЕНЮ",
+    contactUActionText: "БУДЬ ЛАСКА, ЗВ'ЯЖІТЬСЯ З НАМИ ДЛЯ ВСІХ ДЕТАЛЕЙ",
+    thermoAshFillingTextAndImageTitle:
+      "ЕКОЛОГІЧНІСТЬ ТА ДОВГОВІЧНІСТЬ МАТЕРІАЛУ",
+    thermoAshFillingTextAndImageMainText1:
+      " Підшивка з термоясеню є дуже популярним вибором серед багатьох наших клієнтів. Чому так? Бо термоясен має безліч переваг та дуже добре зарекомендував себе за багато років експлуатації.",
+    thermoAshFillingTextAndImageMainText2: " Основні переваги матеріалу:",
+    thermoAshFillingTextAndImageMainText3:
+      "Теплоізоляція: Термоясень має високу теплоізоляційну властивість, що дозволяє зберігати тепло у приміщенні.",
+    thermoAshFillingTextAndImageMainText4:
+      "Екологічність: Термоясень є природним матеріалом, який не містить шкідливих речовин і не викидає в шкідливі речовини.",
+    thermoAshFillingTextAndImageMainText5:
+      "Довговічність: Термоясень відмінно тримається, має високу стійкість до впливу атмосферних умов і механічних пошкоджень.",
+    thermoAshFillingTextAndImageMainText6:
+      "Естетика: Підшивка з термоясену має природний дерев'яний вигляд, що додає природність та комфорт до інтер'єру.",
+    thermoAshFillingTextAndImageMainText7:
+      "Звукоізоляція: Термоясень добре поглинає звуки, що дозволяє створити комфортну акустичну обстановку у приміщенні.",
+    thermoAshFillingTextAndImageMainText8:
+      "Легкість у встановленні: Термоясень легко ріжеться та монтується, що спрощує процес встановлення.",
+    thermoAshFillingTextAndImageMainText9:
+      "Регулювання вологості: Термоясень має властивість регулювати вологість у приміщенні, створюючи комфортні умови для мешканців.",
+    thermoAshFillingTextAndImageMainText10:
+      "Висока антисептичність: Термоясень має відмінні антисептичні властивості, що дозволяють зберігати здоров’я приміщення.",
+    thermoAshFillingTextAndImageMainText11:
+      " Вибір термоясенової підшивки може стати відмінним рішенням для створення комфортного і естетичного інтер'єру у вашому приміщенні.",
+    thermoAshFillingImageAndTextTitle:
+      "БАГАТОФУНКЦІОНАЛЬНІСТЬ ТА КОМФОРТНИЙ МОНТАЖ",
+    thermoAshFillingImageAndTextMainText1:
+      "Термояснева підшивка може бути використана для оздоблення різних типів приміщень, що робить її універсальним вибором для будь-яких дизайнерських проектів",
+    thermoAshFillingImageAndTextMainText2:
+      "Установка може бути швидшою порівняно з іншими видами підшивки, що зекономить час у процесі ремонту чи будівництва, оскільки матеріал легкий і має зручну форму для різання та монтажу.",
+    thermoAshFillingImageAndTextMainText3:
+      "Термоясневий матеріал може бути фарбований або оброблений для досягнення бажаного дизайну та кольору. Також використання подібної підшивки може покращити енергоефективність приміщення шляхом збереження тепла і зниження витрат на опалення.",
   },
 };
 
